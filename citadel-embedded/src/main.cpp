d2617f0fe360911f554c32ff6d9ef762155e0599
David Sharpe <ds0196@uah.edu>
2026-02-22 20:37:03 -0600

    fix up code, fan now works

--- citadel-embedded/src/main.cpp
+++ citadel-embedded/src/main.cpp
@@ -9,7 +9,6 @@
 #include <ESP32Servo.h>
 
 #include <cmath>
-#include <iostream>
 #include <typeinfo>
 
 #include "AstraMisc.h"
@@ -22,7 +21,6 @@
 #define CAN_TX 16
 #define CAN_RX 17
 
-#define FAN_MOTOR_ID 1    // TODO: Needs to be confirmed that this is the correct CAN ID
 #define REV_PWM_MIN 1000  // us  -1.0 duty
 #define REV_PWM_MAX 2000  // us  1.0 duty
 
@@ -68,8 +66,6 @@ int distributorID;
 // Control the NEO550 functioning as the fan motor
 Servo fanMotor;
 
-// Declarations
-void Stop();
 
 void setup() {
     // Servo Pins: 13,14,18,19,22,23,25,26,27
@@ -93,11 +89,23 @@ void setup() {
 
     fanMotor.attach(SPARK_PWM, REV_PWM_MIN, REV_PWM_MAX);
 
+    valve1.write(0);
+    valve2.write(0);
+    valve3.write(0);
+    distributor1.write(0);
+    distributor2.write(0);
+    distributor3.write(0);
+    chemical1.write(0);
+    chemical2.write(0);
+    chemical3.write(0);
+    fanMotor.writeMicroseconds((REV_PWM_MIN + REV_PWM_MAX) / 2);
+
     if (ESP32Can.begin(TWAI_SPEED_1000KBPS, CAN_TX, CAN_RX))
         Serial.println("CAN bus started!");
     else
         Serial.println("CAN bus failed!");
 }
+
 
 void loop() {
     if (millis() - lastServoMoveTime >= servoSpeed) {
@@ -112,6 +120,13 @@ void loop() {
                 servoReference[i]->write(currentServoPos[i]);
             }
         }
+    }
+
+    // Motor control safety timeout- if no command is received in 1 second, shut off the NEO
+    if (millis() - lastCtrlCmd > 1000) {
+        lastCtrlCmd = millis();
+        fanMotor.writeMicroseconds((REV_PWM_MIN + REV_PWM_MAX) / 2);
+        Serial.println("CITADEL Fan Motor - Safety Timeout.");
     }
 
     // Serial commands
@@ -151,67 +166,70 @@ void loop() {
                 vicCAN.relayOff();
             }
         }
-        // Servos are numbered 1-9
-        // To use: servo,[servo number],[degrees]
-        else if (args[0] == "servo") {
-            int servo_id = args[1].toInt() - 1;  // Get servo id
-            int servo_angle = args[2].toInt();
 
-            if (servo_id >= 0 && servo_id < 9) {
-                // Validate the servo values - Servo IDs 6-9 (the chemical servos) should only move 60 degrees
-                // to prevent overextension and other issues
+        // These are for debugging only... as in trying to figure out why the VicCAN commands aren't working.
 
-                if (servo_id >= 1 && servo_id <= 3) {
-                    targetServoPos[servo_id] = (servo_angle <= 60) ? servo_angle : 60;
-                }
-                // Other servos can move the full 180 degrees
-                else {
-                    targetServoPos[servo_id] = servo_angle;
-                }
-            }
-        }
-        // Commands that move all servos in a group
-        //  val = valve servos
-        //  dist = distributor servos
-        //  chem = chemical servos
-        //  all = all servos (might cause power issues)
+        // // Servos are numbered 1-9
+        // // To use: servo,[servo number],[degrees]
+        // else if (args[0] == "servo") {
+        //     int servo_id = args[1].toInt() - 1;  // Get servo id
+        //     int servo_angle = args[2].toInt();
 
-        else if (args[0] == "val") {
-            valve1.write(args[1].toInt());
-            valve2.write(args[1].toInt());
-            valve3.write(args[1].toInt());
-        } else if (args[0] == "dist") {
-            distributor1.write(args[1].toInt());
-            distributor2.write(args[1].toInt());
-            distributor3.write(args[1].toInt());
-        } else if (args[0] == "chem") {
-            if (args[1].toInt() >= 55) {
-                chemical1.write(55);
-                chemical2.write(55);
-                chemical3.write(55);
-            } else {
-                chemical1.write(args[1].toInt());
-                chemical2.write(args[1].toInt());
-                chemical3.write(args[1].toInt());
-            }
-        } else if (args[0] == "all") {
-            valve1.write(args[1].toInt());
-            valve2.write(args[1].toInt());
-            valve3.write(args[1].toInt());
-            distributor1.write(args[1].toInt());
-            distributor2.write(args[1].toInt());
-            distributor3.write(args[1].toInt());
+        //     if (servo_id >= 0 && servo_id < 9) {
+        //         // Validate the servo values - Servo IDs 6-9 (the chemical servos) should only move 60 degrees
+        //         // to prevent overextension and other issues
 
-            if (args[1].toInt() >= 55) {
-                chemical1.write(55);
-                chemical2.write(55);
-                chemical3.write(55);
-            } else {
-                chemical1.write(args[1].toInt());
-                chemical2.write(args[1].toInt());
-                chemical3.write(args[1].toInt());
-            }
-        }
+        //         if (servo_id >= 1 && servo_id <= 3) {
+        //             targetServoPos[servo_id] = (servo_angle <= 60) ? servo_angle : 60;
+        //         }
+        //         // Other servos can move the full 180 degrees
+        //         else {
+        //             targetServoPos[servo_id] = servo_angle;
+        //         }
+        //     }
+        // }
+        // // Commands that move all servos in a group
+        // //  val = valve servos
+        // //  dist = distributor servos
+        // //  chem = chemical servos
+        // //  all = all servos (might cause power issues)
+
+        // else if (args[0] == "val") {
+        //     valve1.write(args[1].toInt());
+        //     valve2.write(args[1].toInt());
+        //     valve3.write(args[1].toInt());
+        // } else if (args[0] == "dist") {
+        //     distributor1.write(args[1].toInt());
+        //     distributor2.write(args[1].toInt());
+        //     distributor3.write(args[1].toInt());
+        // } else if (args[0] == "chem") {
+        //     if (args[1].toInt() >= 55) {
+        //         chemical1.write(55);
+        //         chemical2.write(55);
+        //         chemical3.write(55);
+        //     } else {
+        //         chemical1.write(args[1].toInt());
+        //         chemical2.write(args[1].toInt());
+        //         chemical3.write(args[1].toInt());
+        //     }
+        // } else if (args[0] == "all") {
+        //     valve1.write(args[1].toInt());
+        //     valve2.write(args[1].toInt());
+        //     valve3.write(args[1].toInt());
+        //     distributor1.write(args[1].toInt());
+        //     distributor2.write(args[1].toInt());
+        //     distributor3.write(args[1].toInt());
+
+        //     if (args[1].toInt() >= 55) {
+        //         chemical1.write(55);
+        //         chemical2.write(55);
+        //         chemical3.write(55);
+        //     } else {
+        //         chemical1.write(args[1].toInt());
+        //         chemical2.write(args[1].toInt());
+        //         chemical3.write(args[1].toInt());
+        //     }
+        // }
     }
 
     // CAN
@@ -238,6 +256,27 @@ void loop() {
             Serial.println("Received ping over CAN");
         }
 
+        else if (commandID == CMD_REV_SET_DUTY) {  // Converts duty cycle input into writeMicroseconds range of the NEO
+            if (canData.size() == 1) {
+                lastCtrlCmd = millis();
+                float percent = canData[0] / 100.0;
+                // Limit to 50% duty cycle per Kade's request
+                if (percent < -0.5) {
+                    percent = -0.5;
+                } else if (percent > 0.5) {
+                    percent = 0.5;
+                }
+                int value = map_d(percent, -1.0, 1.0, REV_PWM_MIN, REV_PWM_MAX);
+                fanMotor.writeMicroseconds(value);
+                Serial.print("Setting REV duty to ");
+                Serial.println(value);
+            }
+        }
+        else if (commandID == CMD_REV_STOP) {
+            lastCtrlCmd = millis();
+            fanMotor.writeMicroseconds((REV_PWM_MIN + REV_PWM_MAX) / 2);
+        }
+
         if (commandID == 40) {
             if (canData.size() == 1) {
                 valveID = canData[0];
@@ -252,9 +291,9 @@ void loop() {
                 }
             }
             if (canData.size() == 4) {
-                distributor1.write(canData[0] ? 180 : 0);
-                distributor2.write(canData[1] ? 180 : 0);
-                distributor3.write(canData[2] ? 180 : 0);
+                distributor1.write(canData[0] ? 100 : 0);
+                distributor2.write(canData[1] ? 100 : 0);
+                distributor3.write(canData[2] ? 100 : 0);
             }
             // If -1 is passed in, close all valves
             // Valve movement
@@ -276,37 +315,6 @@ void loop() {
                 chemical2.write(millimetersToMove);
             } else if (tubeID == 2) {
                 chemical3.write(millimetersToMove);
-            }
-        }
-        // Converts duty cycle input into writeMicroseconds range of the NEO
-        if (commandID == 19) {
-            if (canData.size() == 1) {
-                lastCtrlCmd = millis();
-                int value = map_d(canData[0] / 100.0, -1.0, 1.0, REV_PWM_MIN, REV_PWM_MAX);
-                fanMotor.writeMicroseconds(value);
-                Serial.print("Setting REV duty to ");
-                Serial.println(value);
-            }
-        }
-
-        // Motor control safety timeout- if no command is received in 2 seconds, shut off the NEO
-        if (millis() - lastCtrlCmd > 2000) {
-            lastCtrlCmd = millis();
-            fanMotor.write(0);
-        } else if (commandID == CMD_REV_STOP) {
-            lastCtrlCmd = millis();
-            fanMotor.write((REV_PWM_MIN + REV_PWM_MAX) / 2);
-        } else if (commandID == CMD_REV_IDENTIFY) {
-            if (canData.size() == 1) {
-                COMMS_UART.print("rev_id,");
-                COMMS_UART.println(canData[0]);
-            }
-        } else if (commandID == CMD_REV_IDLE_MODE) {
-            if (canData.size() == 1) {
-                if (canData[0] == 0)
-                    COMMS_UART.println("brake,off");
-                else if (canData[0] == 1)
-                    COMMS_UART.println("brake,on");
             }
         }
     }
